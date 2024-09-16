acdbedecd78f47d5309ee8c4635de39e5d5415a1
Pablo V <pablo.valdesogo@gmail.com>
2024-09-16 17:47:01 -0300

    commit inicial

---
+++ transportes/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import ConductorList from './components/ConductoresList';
+
+function App() {
+  return (
+    <div>
+      <ConductorList />
+    </div>
+  );
+}
+
+export default App;
